c286e01115db69f5d7a8af1a0a004e22fcd727fd
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2022-12-09 16:43:18 +0100

    Add files via upload

---
+++ TestMaui/Models/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace TestMaui.Models
+{
+    public class Item
+    {
+        private string name;
+
+        private bool isComplete;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                OnPropChange(name, value);
+            }
+        }
+
+        public bool IsComplete
+        {
+            get => isComplete;
+            set
+            {
+                OnPropChange(IsComplete, value);
+            }
+        }
+
+        public Item(string name, bool isComplete)
+        {
+            this.name = name;
+            this.isComplete = isComplete;
+        }
+
+        private void OnPropChange(object oldVal, object newVal, [CallerMemberName] string propName = "")
+        {
+            Console.WriteLine($"PropChanged: {propName}: {oldVal} -> {newVal}");
+        }
+    }
+}
+
---
+++ TestMaui/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TestMaui.Models;
+
+namespace TestMaui.ViewModels
+{
+    public partial class MainPageViewModel
+    {
+        public List<Item> Items { get; }
+
+        public MainPageViewModel()
+        {
+            var item1 = new Item("Item1", false);
+            var item2 = new Item("Item2", true);
+
+            Items = new List<Item>();
+
+            Items.Add(item1);
+            Items.Add(item2);
+        }
+    }
+}
+
